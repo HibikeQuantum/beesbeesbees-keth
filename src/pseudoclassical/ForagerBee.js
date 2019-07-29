91619b1bec06ee1410e421b1f5e297b8d63caaba
yobs0814 <yobs0814@gmail.com>
2019-07-29 17:59:26 +0900

    ing

--- src/pseudoclassical/ForagerBee.js
+++ src/pseudoclassical/ForagerBee.js
@@ -1,7 +1,20 @@
 var Bee = require('./Bee');
 
 var ForagerBee = function () {
-
+    Bee.call(this);
+    this.age = 10;
+    this.job = 'find pollen';
+    this.canFly = true;
+    this.treasureChest = [];
 };
 
+ForagerBee.prototype = Object.create(Bee.prototype);
+ForagerBee.prototype.constructor = ForagerBee;
+
+ForagerBee.prototype.forage = function(input){
+    this.treasureChest.push(input);
+}
+
+
 module.exports = ForagerBee;
+
--- src/pseudoclassical/HoneyMakerBee.js
+++ src/pseudoclassical/HoneyMakerBee.js
@@ -1,7 +1,22 @@
 var Bee = require('./Bee');
 
 var HoneyMakerBee = function () {
-
+    Bee.call(this);
+    this.age = 10;
+    this.job = 'make honey';
+    this.honeyPot = 0;
 };
+
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.makeHoney = function(){
+    this.honeyPot += 1;
+}
+
+HoneyMakerBee.prototype.giveHoney = function(){
+    this.honeyPot -= 1;
+}
+
 
 module.exports = HoneyMakerBee;
